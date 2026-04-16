263f26b9464c58ba2cc828a454064043f71e3bd7
s1210724 <s1210724@student.windesheim.nl>
2026-04-16 20:25:14 +0200

    feat: added option to join, leave and rejoin session

--- src/sockets/gameSocket.js
+++ src/sockets/gameSocket.js
@@ -20,30 +20,79 @@ module.exports = (io) => {
             }
         });
 
+        socket.on("joinSession", (data, ack) => {
+            const { sessionId, password } = data;
+            const session = sessions[sessionId];
+
+            if (!session) {
+                if (typeof ack === "function") {
+                    ack({ error: "Session not found" });
+                }
+                return;
+            }
+
+            if (session.password !== password) {
+                if (typeof ack === "function") {
+                    ack({ error: "Incorrect password" });
+                }
+                return;
+            }
+
+            socket.join(sessionId);
+            if (typeof ack === "function") {
+                ack('succ6');
+            }
+        });
+
         socket.on("migrateToSession", () => {
-            const lobby = getLobbyBySocketId(socket);
-            const sessionId = sessionService.migrateLobbyToSession(lobby, sessions);
+            const room = getLobbyBySocketId(socket);
+            if (!room || room.type !== "lobby") {
+                return;
+            }
+
+            const sessionId = sessionService.migrateLobbyToSession(room.data, sessions);
             socket.emit("sessionMigrated", {id: sessionId, password: sessions[sessionId].password});
         });
 
         socket.on("disconnecting", () => {
-            const lobby = getLobbyBySocketId(socket);
-            if (!lobby) {
+            const room = getLobbyBySocketId(socket);
+            if (!room) {
+                return;
+            }
+
+            if (room.type !== "lobby") {
                 return;
             }
 
             // remove the player from the lobby and delete the lobby if it becomes empty
-            lobbyService.leaveLobby(lobbys, lobby.id, socket.id);
+            lobbyService.leaveLobby(lobbys, room.data.id, socket.id);
 
             // notify remaining players in the lobby about the departure
-            io.to(lobby.id).emit("playerLeft", socket.id);
+            io.to(room.data.id).emit("playerLeft", socket.id);
         });
     });
 };
 
 function getLobbyBySocketId(socket) {
-    const joinedLobbyIds = [...socket.rooms].filter((roomId) => roomId !== socket.id);
-    const lobbyId = joinedLobbyIds.find((roomId) => Boolean(lobbys[roomId]));
-    const lobby = lobbys[lobbyId];
-    return lobby;
+    const joinedRoomIds = [...socket.rooms].filter((roomId) => roomId !== socket.id);
+
+    const lobbyId = joinedRoomIds.find((roomId) => Boolean(lobbys[roomId]));
+    if (lobbyId) {
+        return { 
+            type: "lobby",
+            id: lobbyId,
+            data: lobbys[lobbyId],
+        };
+    }
+
+    const sessionId = joinedRoomIds.find((roomId) => Boolean(sessions[roomId]));
+    if (sessionId) {
+        return {
+            type: "session",
+            id: sessionId,
+            data: sessions[sessionId],
+        };
+    }
+
+    return null;
 }
